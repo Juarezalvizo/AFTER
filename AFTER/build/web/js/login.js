18953c6493a6fd8aeedc5536a17ba3d1561c0b43
Angel Juarez <21000381@alumnos.utleon.edu.mx>
2023-03-21 16:54:19 -0600

    Actualizacion de AFTER

---
+++ AFTER/build/web/js/login.js
@@ -0,0 +1,22 @@
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/JavaScript.js to edit this template
+ */
+
+function loginn()
+{
+    window.location.replace("http://localhost:8080/AFTER/principal.html");
+}
+function logout()
+{
+    window.location.replace('http://localhost:8080/OpticaQualite_v2/');
+}
+
+//// <script>
+//// JavaScript
+//const botonIngresar = document.getElementById('btnIngresar');
+//
+//botonIngresar.addEventListener('click', function() {//
+////  window.location.href = ';
+////});
+//</script>
